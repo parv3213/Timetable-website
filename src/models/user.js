0e5e87fd6c3779333225ba3fc7af0ac8085074d3
parv3213 <parv3213@gmail.com>
2020-07-02 19:46:40 +0530

    made user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-	name: String,
+	//TODO set password using jwt
+	name: { type: String, required: true },
+	email: { type: String, required: true },
+	rno: { type: Number, required: true },
+	coreMember: { type: Boolean, default: false },
 });
 
 const User = mongoose.model("User", userSchema);
-
 module.exports = User;
