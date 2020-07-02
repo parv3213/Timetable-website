a984ac9a934309fd2e3c8c4d1d9396a5be2062f3
parv3213 <parv3213@gmail.com>
2020-07-03 02:59:44 +0530

    added root route

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ require("./db/mongoose");
 // Import Routes
 const signupRoute = require("./routes/signup");
 const loginRoute = require("./routes/login");
+const rootRoute = require("./routes/root");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(signupRoute);
 app.use(loginRoute);
+app.use(rootRoute);
 
 // App connection
 app.listen(port, () => {
