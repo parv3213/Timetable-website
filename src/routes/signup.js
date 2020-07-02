4bc06c0a4baaa7dd0acd5a287558e4db9cd84eeb
parv3213 <parv3213@gmail.com>
2020-07-02 23:23:38 +0530

    corrected code

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/user");
+const { User } = require("../models/user");
 const router = new express.Router();
 
 router.get("/signup", (req, res) => {
