08bc16978e1cfe19b26f3af9170c5e5d14dd711b
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-07-03 11:31:10 +0530

    Updated for checking existing emails&password

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -7,18 +7,32 @@ router.get("/signup", (req, res) => {
 });
 
 router.post("/signup", async (req, res) => {
-	const user = new User({
-		name: req.body.name,
-		email: req.body.email,
-		rno: req.body.rno,
-	});
-
-	try {
-		await user.save();
-		res.send(user);
-	} catch (e) {
-		res.status(500).send(e.message); // TODO correct status code
+	if(req.body.password == req.body.confpass) {
+		User.findOne({ email: req.body.email }, async (err, data) => {
+			if(!data) {
+				const user = new User({
+					name: req.body.name,
+					email: req.body.email,
+					rno: req.body.rno,
+					password: req.body.password,
+					confpass: req.body.confpass
+				});
+		
+				try {
+					await user.save();
+					res.send(user);
+				} catch (e) {
+					res.status(500).send(e.message); // TODO correct status code
+				}
+			}else {
+				res.send("Email already in use");
+			}
+		});
+		
+	}else {
+		res.send("Passwords don't match");
 	}
+	
 });
 
 module.exports = router;
