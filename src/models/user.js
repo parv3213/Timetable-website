06e8a5f0d055a968d92ca57ac00a8abbdea25ccc
parv3213 <parv3213@gmail.com>
2020-07-04 08:20:11 +0530

    Change in structure using middleware

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 // FIXME Removed Joi and added validator (remove comment after you see it)
 
 const userSchema = new mongoose.Schema({
@@ -17,6 +18,14 @@ const userSchema = new mongoose.Schema({
 	rno: { type: Number, required: true },
 	password: { type: String, required: true },
 	coreMember: { type: Boolean, default: false },
+});
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+	const user = this;
+	if (user.isModified("password")) {
+		user.password = await bcrypt.hash(user.password, 10);
+	}
 });
 
 const User = mongoose.model("User", userSchema);
--- src/routes/signup.js
+++ src/routes/signup.js
@@ -5,35 +5,45 @@ const User = require("../models/user");
 const router = new express.Router();
 
 router.get("/signup", (req, res) => {
-	res.send("HI"); //FIXME We will add the ui page here
+	res.send("HI"); //TODO We will add the ui page here
 });
 
-router.post("/signup", (req, res) => {
-	User.findOne({ email: req.body.email }, (err, data) => {
-		if (!data) {
-			bcrypt.hash(req.body.password, 10, async (err, hash) => {
-				if (err) {
-					return res.status(500).json({ error: err });
-				} else {
-					const user = new User({
-						name: req.body.name,
-						email: req.body.email,
-						rno: req.body.rno,
-						password: hash,
-					});
+router.post("/signup", async (req, res) => {
+	try {
+		let user = await User.findOne({ email: req.body.email });
+		if (user) throw new Error("Email Id already exists");
+		user = new User(req.body);
+		await user.save();
+		res.send(user);
+	} catch (e) {
+		res.status(500).send({ e: e.message }); //FIXME correct the status code
+	}
+	// FIXME understand the above code and then delete this and below comments
+	// User.findOne({ email: req.body.email }, (err, data) => {
+	// 	if (!data) {
+	// 		bcrypt.hash(req.body.password, 10, async (err, hash) => {
+	// 			if (err) {
+	// 				return res.status(500).json({ error: err });
+	// 			} else {
+	// 				const user = new User({
+	// 					name: req.body.name,
+	// 					email: req.body.email,
+	// 					rno: req.body.rno,
+	// 					password: hash,
+	// 				});
 
-					try {
-						await user.save();
-						res.status(201).send(user);
-					} catch (e) {
-						res.status(500).send(e.message); // TODO correct status code
-					}
-				}
-			});
-		} else {
-			res.send("Email already in use");
-		}
-	});
+	// 				try {
+	// 					await user.save();
+	// 					res.status(201).send(user);
+	// 				} catch (e) {
+	// 					res.status(500).send(e.message);
+	// 				}
+	// 			}
+	// 		});
+	// 	} else {
+	// 		res.send("Email already in use");
+	// 	}
+	// });
 });
 
 module.exports = router;
