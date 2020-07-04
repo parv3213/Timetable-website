9f6d7a0bb1356921085fa4c70af5c0576570e49d
parv3213 <parv3213@gmail.com>
2020-07-04 07:26:02 +0530

    npm

--- src/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const url = "mongodb+srv://User:3axg96GLRJ!_2Eh@cluster0.uj2h1.mongodb.net/timetable?retryWrites=true&w=majority";
-
-mongoose.connect(url, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
