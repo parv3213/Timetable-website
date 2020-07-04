41fbdb87be6c49df3b8f1171dca5ae3464ff36e9
parv3213 <parv3213@gmail.com>
2020-07-04 07:22:38 +0530

    added env path to signup

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: "./src/config/.env" });
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
@@ -8,33 +9,31 @@ router.get("/signup", (req, res) => {
 });
 
 router.post("/signup", (req, res) => {
-		User.findOne({ email: req.body.email }, (err, data) => {
-			if(!data) {
-				bcrypt.hash(req.body.password, 10, async (err, hash) => {
-					if(err) {
-						return res.status(500).json({ error: err});
-					}else {
-						const user = new User({
-							name: req.body.name,
-							email: req.body.email,
-							rno: req.body.rno,
-							password: hash
-						});
-				
-						try {
-							await user.save();
-							res.status(201).send(user);
-						} catch (e) {
-							res.status(500).send(e.message); // TODO correct status code
-						}
+	User.findOne({ email: req.body.email }, (err, data) => {
+		if (!data) {
+			bcrypt.hash(req.body.password, 10, async (err, hash) => {
+				if (err) {
+					return res.status(500).json({ error: err });
+				} else {
+					const user = new User({
+						name: req.body.name,
+						email: req.body.email,
+						rno: req.body.rno,
+						password: hash,
+					});
+
+					try {
+						await user.save();
+						res.status(201).send(user);
+					} catch (e) {
+						res.status(500).send(e.message); // TODO correct status code
 					}
-				});
-				
-			}else {
-				res.send("Email already in use");
-			}
-		});
-	
+				}
+			});
+		} else {
+			res.send("Email already in use");
+		}
+	});
 });
 
 module.exports = router;
