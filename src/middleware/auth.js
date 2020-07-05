0bfb48a325f5573f042041497b995f0653f79ad8
parv3213 <parv3213@gmail.com>
2020-07-05 11:09:50 +0530

    changed error message

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,7 +8,7 @@ const auth = async (req, res, next) => {
 		const token = req.header("Authorization").replace("Bearer ", "");
 		const decoded = jwt.verify(token, jwtKey);
 		const user = await User.findOne({ _id: decoded._id, "tokens.token": token });
-		if (!user) throw new Error();
+		if (!user) throw new Error("User does not exists");
 		req.token = token;
 		req.user = user;
 		next();
